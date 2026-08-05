30c7b3b5cf201665e4d94617389c76e4c294b37d
venkataped@gmail.com <venkatasai.ped@ibm.com>
2026-08-05 13:11:34 +0530

    websockets: Actual Solution for Connect4 Game Part1

--- app.py
+++ app.py
@@ -13,25 +13,43 @@ async def handler(websocket):
     curr_player = PLAYER1
     async for message in websocket:
         event = json.loads(message)
-        game.play(curr_player, event["column"])
-        last_player = game.last_player
-        curr_player = PLAYER2 if last_player==PLAYER1 else PLAYER1
-        for player, column, row in game.moves:
-            if (not game.winner):
-                event = {
-                    "type": "play",
-                    "player": player,
-                    "column": column,
-                    "row": row,
-                }
-                await websocket.send(json.dumps(event))
-                await asyncio.sleep(0.1)
-            else:
-                event = {
-                    "type": "win",
-                    "player": game.winner,
-                }
-                await websocket.send(json.dumps(event))
+        is_play = event["type"] == "play"
+        if is_play:
+            column = event["column"]
+        try:
+            row = game.play(curr_player, column)
+        except Exception as ex:
+            event = {
+                "type": "error",
+                "message": str(ex),
+            }
+            await websocket.send(json.dumps(event))
+            continue
+        if (not game.winner):
+            event = {
+                "type": "play",
+                "player": curr_player,
+                "column": column,
+                "row": row,
+            }
+            await websocket.send(json.dumps(event))
+            await asyncio.sleep(0.3)
+            last_player = game.last_player
+            curr_player = PLAYER2 if last_player==PLAYER1 else PLAYER1
+        else:
+            event = {
+                "type": "play",
+                "player": curr_player,
+                "column": column,
+                "row": row,
+            }
+            await websocket.send(json.dumps(event))
+            await asyncio.sleep(0.3)
+            event = {
+                "type": "win",
+                "player": game.winner,
+            }
+            await websocket.send(json.dumps(event))
 
 async def main():
     server = await serve(handler, "", 8001) # Starts a websocket server
