ad6a534239d4449fae32f9d6bbe63be18bf268a4
venkataped@gmail.com <venkatasai.ped@ibm.com>
2026-08-09 18:30:49 +0530

    websockets: removed old handler code snippet

--- app.py
+++ app.py
@@ -13,49 +13,6 @@ import secrets
 
 JOIN = {}
 
-# async def handler(websocket):
-#     game = Connect4()
-#     curr_player = PLAYER1
-#     async for message in websocket:
-#         event = json.loads(message)
-#         is_play = event["type"] == "play"
-#         if is_play:
-#             column = event["column"]
-#         try:
-#             row = game.play(curr_player, column)
-#         except Exception as ex:
-#             event = {
-#                 "type": "error",
-#                 "message": str(ex),
-#             }
-#             await websocket.send(json.dumps(event))
-#             continue
-#         if (not game.winner):
-#             event = {
-#                 "type": "play",
-#                 "player": curr_player,
-#                 "column": column,
-#                 "row": row,
-#             }
-#             await websocket.send(json.dumps(event))
-#             await asyncio.sleep(0.3)
-#             last_player = game.last_player
-#             curr_player = PLAYER2 if last_player==PLAYER1 else PLAYER1
-#         else:
-#             event = {
-#                 "type": "play",
-#                 "player": curr_player,
-#                 "column": column,
-#                 "row": row,
-#             }
-#             await websocket.send(json.dumps(event))
-#             await asyncio.sleep(0.3)
-#             event = {
-#                 "type": "win",
-#                 "player": game.winner,
-#             }
-#             await websocket.send(json.dumps(event))
-
 async def start(websocket):
     game = Connect4()
     connected = {websocket}
